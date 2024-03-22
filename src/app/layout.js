72dea81c6083b1813d644d6780d7f67e2f8a53db
Filiberto Zurita <fmasz6@gmail.com>
2024-03-21 21:39:39 -0400

    google verification try 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Rafael Zurita | Porfolio',
   description: 'Tutor Transitioning to Data and Web Development',
+  // Other meta tags as needed
+  verification: {
+    googleSiteVerification: 'vL-pMo5xJB5P3PJHL9ZevQS1ApJOmxOWuhjXvBctgBQ',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="google-site-verification" content="vL-pMo5xJB5P3PJHL9ZevQS1ApJOmxOWuhjXvBctgBQ" />
       <body className={inter.className}>{children}</body>
     </html>
   )
