70a8c8a028e66de052d41a36450e7ba4aed62d8c
Filiberto Rafael Zurita Abad <68559567+filiberto-rafael-zurita-abad@users.noreply.github.com>
2024-10-03 19:25:02 -0400

    hello this is a test

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
+        hello this is a test
         <HeroSection />
         <AchievementsSection />
         <AboutSection />
